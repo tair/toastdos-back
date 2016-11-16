3c5835fbe5c68f48b3a5666e788231a52547ae60
Joseph DeSimpliciis <jcdesimp@gmail.com>
2016-11-15 19:57:12 -0500

    Added message after generating file.

--- scripts/generateOrcidFile.js
+++ scripts/generateOrcidFile.js
@@ -10,7 +10,8 @@ const path      = require('path');
 let jsonStructure = {
     "client_id": "APP-EXAMPLE",
     "client_secret": "12345678-abcdef-clint-secret"
-}
+};
 
 fs.writeFileSync(path.join(DESTINATION_DIR, FILENAME), JSON.stringify(jsonStructure, null, 4));
 
+console.log("File created in 'resouces/orcid_app_info.json', be sure to replace the values with values from your ORCID account app.");
