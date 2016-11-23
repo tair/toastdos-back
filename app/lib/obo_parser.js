ab7e921d3cb24d681ec3e7d547d8301d57743683
James Moretti <thekillerdonut1@aol.com>
2016-11-23 05:51:25 -0500

    Obo parser implemented as a state machine.
    This is probably over-engineered, frankly.

--- app/lib/obo_parser.js
+++ app/lib/obo_parser.js
@@ -5,6 +5,12 @@ const stream = require('stream');
 const BLOCK_START = /^\[(.+)]$/;
 const KEY_VALUE   = /(\w+): (.*)/;
 
+const State = Object.freeze({
+	HEADER: 'Header',
+	IGNORE: 'Ignore',
+	PARSING: 'Parsing'
+});
+
 class OboParser extends stream.Transform {
 
 	constructor(headerParser) {
@@ -13,31 +19,75 @@ class OboParser extends stream.Transform {
 			writableObjectMode: true
 		});
 		this.headerParser = headerParser;
-		this.isTerm = true;
+		this.state = State.HEADER;
 		this.currentTerm = {};
 	}
 
-	_transform(chunk, encoding, done) {
+	_transform(chunk, encoding, next) {
 		chunk.toString().split('\n').forEach(line => {
 
-			let blockStart = line.match(BLOCK_START);
+			switch (this.state) {
 
-			if (blockStart) {
-				this.currentTerm = {};
-				this.isTerm = (blockStart[1] === 'Term');
-			}
-			else if (line.length === 0) {
-				this.push(this.currentTerm);
-			}
-			else {
-				let parts = line.match(KEY_VALUE);
-				let key = parts[1];
-				let val = parts[2];
-				this.currentTerm[key] = val;
+				// Parse initial header until we see a new line
+				case State.HEADER:
+					let headerParts = line.match(KEY_VALUE);
+					if (headerParts) {
+						this.currentTerm[headerParts[1]] = headerParts[2];
+					}
+					else {
+						// Pass parsed header into the callback
+						if (this.headerParser) {
+							this.headerParser(this.currentTerm);
+						}
+						this.currentTerm = {};
+
+						// Is this a newline or the start of a block? If there is no header, this is the entry point (sorry, this is filthy, I know.)
+						let blockStart = line.match(BLOCK_START);
+						if (blockStart) {
+							if (blockStart[1] === 'Term') {
+								this.state = State.PARSING;
+							} else {
+								this.state = State.IGNORE;
+							}
+						}
+						else {
+							// This was a newline, defer to PARSING block
+							this.state = State.PARSING;
+						}
+					}
+					break;
+
+
+				case State.PARSING:
+					let blockStart2 = line.match(BLOCK_START);
+					if (blockStart2 && blockStart2[1] !== 'Term') {
+						// If this block isn't a term, ignore everything until the Next term
+						this.state = State.IGNORE;
+					}
+					else if (line.length === 0) {
+						// Finish the current term when we see a newline
+						this.push(this.currentTerm);
+						this.currentTerm = {};
+					}
+					else {
+						// Save the current line's key-value to the term
+						let parts = line.match(KEY_VALUE);
+						this.currentTerm[parts[1]] = parts[2];
+					}
+					break;
+
+
+				case State.IGNORE:
+					// Ignore data until we find the next term
+					let blockStart3 = line.match(BLOCK_START);
+					if (blockStart3 && blockStart3[1] === 'Term') {
+						this.state = State.PARSING;
+					}
+					break;
 			}
 		});
 
-		done();
+		next();
 	}
 }
 
