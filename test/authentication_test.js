884397958edea692ca421ff50f64e65c6fe62269
James Moretti <thekillerdonut1@aol.com>
2016-12-05 22:15:14 -0500

    Tests for user authentication middleware

--- test/authentication_test.js
+++ test/authentication_test.js
@@ -11,27 +11,27 @@ const testdata = require('../seeds/test_data.json');
 
 describe('Authentication middleware', function() {
 
+	let testToken = '';
+
+	// Make a token so tests can authenticate
+	before('Generate test JWT', function(done) {
+		let authenticatedUser = testdata.users[0];
+		auth.signToken({user_id: authenticatedUser.id}, (err, newToken) => {
+			chai.expect(err).to.be.null;
+			testToken = newToken;
+			done();
+		});
+	});
+
+	before('Setup SQLite memory database', function() {
+		return knex.migrate.latest();
+	});
+
+	beforeEach('Populate SQLite memory DB with fresh test data', function() {
+		return knex.seed.run();
+	});
+
 	describe('JWT basic authentication', function() {
-
-		let testToken = '';
-
-		// Make a token so tests can authenticate
-		before('Generate test JWT', function(done) {
-			let authenticatedUser = testdata.users[0];
-			auth.signToken({user_id: authenticatedUser.id}, (err, newToken) => {
-				chai.expect(err).to.be.null;
-				testToken = newToken;
-				done();
-			});
-		});
-
-		before('Setup SQLite memory database', function() {
-			return knex.migrate.latest();
-		});
-
-		beforeEach('Populate SQLite memory DB with fresh test data', function() {
-			return knex.seed.run();
-		});
 
 		it('A well formed authentication header is accepted', function(done) {
 			let testUser = testdata.users[0];
@@ -40,7 +40,6 @@ describe('Authentication middleware', function() {
 				.set({Authorization: 'Bearer ' + testToken})
 				.end((err, res) => {
 					chai.expect(res.status).to.equal(200);
-					chai.expect(res.body).to.contain(testUser);
 					done();
 				});
 		});
@@ -101,6 +100,33 @@ describe('Authentication middleware', function() {
 
 	});
 
-	//describe('User authentication');
+	describe('User authentication', function() {
+
+		it('Successfully retrieves user whose ID matches ID in JWT', function(done) {
+			let testUser = testdata.users[0];
+			chai.request(server)
+				.get('/api/user/' + testUser.id)
+				.set({Authorization: 'Bearer ' + testToken})
+				.end((err, res) => {
+					chai.expect(res.status).to.equal(200);
+					chai.expect(res.body).to.contain(testUser);
+					done();
+				});
+		});
+
+		it('Cannot retrieve a user whose ID doesnt match ID in JWT', function(done) {
+			let testUser = testdata.users[0];
+			auth.signToken({user_id: 'fakeid'}, (tokenerr, token) => {
+				chai.request(server)
+					.get('/api/user/' + testUser.id)
+					.set({Authorization: 'Bearer ' + token})
+					.end((err, res) => {
+						chai.expect(res.status).to.equal(401);
+						done();
+					});
+			});
+		});
+
+	});
 
 });
