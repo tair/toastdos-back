04c6a3930c0fefdfb8c2c6cc73bf14dc753f4f40
Jean Cheng <Mochi@Jeans-MBP.wireless.rit.edu>
2016-12-09 17:10:54 -0500

    added keywordtype endpoint and one test

--- test/keyword_type_test.js
+++ test/keyword_type_test.js
@@ -9,16 +9,28 @@ const knex    = require('../app/lib/bookshelf').knex;
 
 const testdata = require('../seeds/test_data.json');
 
+
 describe('KeywordType Controller', function() {
 
-	before('Setup SQLite memory database', function() {
-		return knex.migrate.latest();
-	});
+    before('Setup SQLite memory database', function() {
+        return knex.migrate.latest();
+    });
 
-	beforeEach('Populate SQLite memory DB with fresh test data', function() {
-		return knex.seed.run();
-	});
-
-	it('Successfully retrieves all KeywordTypes in the database');
+    beforeEach('Populate SQLite memory DB with fresh test data', function() {
+        return knex.seed.run();
+    });
+    describe('GET Keyword Type', function(){
+        it('Successfully retrieves KeywordType by id in the database', function(done) {
+            let testdata=testdata.keyword_types[0];
+            chai.request(server)
+                .get(testdata.id)
+                .end((err, res) => {
+                    chai.expect(res.status).to.equal(200);
+                    chai.expect(res.body).to.contain(testdata);
+                    done();
+                });
+        });
+    });  
+    
 
 });
