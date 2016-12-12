3174b1ebe7391bdf08492cb1e02819d53de0e001
James Moretti <thekillerdonut1@aol.com>
2016-12-12 18:01:43 -0500

    DOI validator with tests

--- app/lib/publication_id_validator.js
+++ app/lib/publication_id_validator.js
@@ -16,7 +16,12 @@ const DOI_VALIDATOR = /^(10\.\d{4,9}\/.+)$/;
  */
 const PUBMED_VALIDATOR = /^(\d+)$/;
 
+const DOI_BASE_URL = 'http://doi.org/api/handles/';
 const PUBMED_BASE_URL = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&retmode=json&id=';
+
+function doiUrl(doi) {
+	return `${DOI_BASE_URL}${doi}`;
+}
 
 function pubmedUrl(pmid) {
 	return `${PUBMED_BASE_URL}${pmid}`;
@@ -31,6 +36,28 @@ function isPubmedId(text) {
 	return !!PUBMED_VALIDATOR(text);
 }
 
+
+/**
+ * Searches DOI registry to see if the provided DOI actually
+ * belongs to an existing publication
+ * @param doi
+ */
+function validateDOI(doi) {
+	return new Promise((resolve, reject) => {
+		request.get(doiUrl(doi), (error, response, bodyJson) => {
+			let body = JSON.parse(bodyJson);
+
+			// www.doi.org is silly and redefines standard HTTP error codes.
+			// See http://www.doi.org/factsheets/DOIProxy.html
+			// All you need to know is responseCode '1' means '200 OK'.
+			if (body.responseCode === 1) {
+				resolve(true);
+			} else {
+				reject(new Error('Provided DOI did not match any publications'));
+			}
+		});
+	});
+}
 
 /**
  * Searches the National Center for Biotechnology Information (NCBI)
@@ -56,5 +83,6 @@ function validatePubmedId(pmid) {
 module.exports = {
 	isDOI,
 	isPubmedId,
+	validateDOI,
 	validatePubmedId
 };
--- test/publication_id_validator_test.js
+++ test/publication_id_validator_test.js
@@ -10,7 +10,7 @@ describe('Publication ID Validator', function() {
 		const realPubmedId = 999999;
 		return validator.validatePubmedId(realPubmedId)
 			.catch(() => {
-				throw new Error('Valid Pubmed ID should not cause error');
+				throw new Error('Valid Pubmed ID threw an error');
 			});
 	});
 
@@ -18,13 +18,26 @@ describe('Publication ID Validator', function() {
 		const badPubmedId = 999999999;
 		validator.validatePubmedId(badPubmedId)
 			.then(() => {
-				throw new Error('Invalid Pubmed ID should not return successfully');
+				throw new Error('Invalid Pubmed ID returned successfully');
 			})
 			.catch(() => done());
 	});
 
-	it('Existing DOI returns success');
+	it('Existing DOI returns success', function() {
+		const realDOI = '10.1594/GFZ.GEOFON.gfz2009kciu';
+		return validator.validateDOI(realDOI)
+			.catch(() => {
+				throw new Error('Valid DOI threw an error');
+			});
+	});
 
-	it('Non-existing DOI throws error');
+	it('Non-existing DOI throws error', function(done) {
+		const badDOI = '10.1234/IM.A.FAKE.DOI';
+		validator.validateDOI(badDOI)
+			.then(() => {
+				throw new Error('Invalid DOI returned successfully');
+			})
+			.catch(() => done());
+	});
 
 });
