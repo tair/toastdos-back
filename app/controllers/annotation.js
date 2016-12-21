07ba25d19d3db0b0e0e4d7e3ac134ae4e835bff6
JDonut <thekillerdonut1@aol.com>
2016-12-21 03:04:59 -0500

    Controller for adding all the parts of a new annotation

--- app/controllers/annotation.js
+++ app/controllers/annotation.js
@@ -2,6 +2,13 @@
 
 const _ = require('lodash');
 
+const publicationValidator = require('../lib/publication_id_validator');
+
+const Publication        = require('../models/publication');
+const GeneTermAnnotation = require('../models/gene_term_annotation');
+const GeneGeneAnnotation = require('../models/gene_gene_annotation');
+const CommentAnnotation  = require('../models/comment_annotation');
+const Annotation         = require('../models/annotation');
 
 /**
  *
@@ -12,22 +19,22 @@ const _ = require('lodash');
 function createAnnotation(req, res, next) {
 
 	// Step 1: Ensure no extraneous data was attached to the request
-	const baseAllowedFields     = ['publication_id', 'status_id', 'submitter_id', 'locus_id', 'type'];
+	const baseAllowedFields     = ['publication_id', 'status_id', 'submitter_id', 'locus_id', 'annotation_type'];
 	const geneTermAllowedFields = ['method_id', 'keyword_id', 'evidence_id'];
 	const geneGeneAllowedFields = ['locus2_id', 'method_id'];
 	const commentAllowedFields  = ['text'];
 
 	let requiredFields;
-	if (req.body.type === 'gene_term_annotation') {
+	if (req.body.annotation_type === 'gene_term_annotation') {
 		requiredFields = baseAllowedFields.concat(geneTermAllowedFields);
 	}
-	else if (req.body.type === 'gene_gene_annotation') {
+	else if (req.body.annotation_type === 'gene_gene_annotation') {
 		requiredFields = baseAllowedFields.concat(geneGeneAllowedFields);
 	}
-	else if (req.body.type === 'comment_annotation') {
+	else if (req.body.annotation_type === 'comment_annotation') {
 		requiredFields = baseAllowedFields.concat(commentAllowedFields);
 	}
-	else if (!req.body.type) {
+	else if (!req.body.annotation_type) {
 		return res.status(400)
 			.json({
 				error: 'BadRequest',
@@ -38,7 +45,7 @@ function createAnnotation(req, res, next) {
 		return res.status(400)
 			.json({
 				error: 'BadRequest',
-				message : 'Unrecognized type ' + res.body.type
+				message : 'Unrecognized annotation_type ' + res.body.annotation_type
 			});
 	}
 
@@ -51,8 +58,69 @@ function createAnnotation(req, res, next) {
 			});
 	}
 
+	// Step 2: Validate the data
+	if (!publicationValidator.isDOI(req.body.publication_id)
+		&& !publicationValidator.isPubmedId(req.body.publication_id)) {
+		return res.status(400)
+			.json({
+				error: 'BadRequest',
+				message: 'Given publication ID is not a DOI or Pubmed ID'
+			});
+	}
 
+	// TODO currently we only have DOI/Pubmed ID validation. Come back to this later
 
+	// TODO evidence_id is required for GeneTermAnnotations under certain circumstances
+
+	// Step 3: Insert the data
+
+	// Identify publication type and add new publication
+	let newPublication;
+	if (publicationValidator.isDOI(req.body.publication_id)) {
+		newPublication = {doi: req.body.publication_id};
+	} else if (publicationValidator.isPubmedId(req.body.publication_id)) {
+		newPublication = {pubmed_id: req.body.publication_id};
+	}
+	let publicationPromise = Publication.forge(newPublication).save();
+
+	// Identify sub-Annotation type and add record
+	let subAnnotationPromise;
+	if (req.body.annotation_type === 'gene_term_annotation') {
+		let newSubAnnotation = _.pick(req.body, geneTermAllowedFields);
+		subAnnotationPromise = GeneTermAnnotation.forge(newSubAnnotation).save();
+	}
+	else if (req.body.annotation_type === 'gene_gene_annotation') {
+		let newSubAnnotation = _.pick(req.body, geneGeneAllowedFields);
+		subAnnotationPromise = GeneGeneAnnotation.forge(newSubAnnotation).save();
+	}
+	else if (req.body.annotation_type === 'comment_annotation') {
+		let newSubAnnotation = _.pick(req.body, commentAllowedFields);
+		subAnnotationPromise = CommentAnnotation.forge(newSubAnnotation).save();
+	}
+
+	// With dependencies created, now add the Annotation itself
+	Promise.all([publicationPromise, subAnnotationPromise])
+		.then(addedRows => {
+			let addedPublication = addedRows[0].toJSON();
+			let addedSubAnnotation = addedRows[1].toJSON();
+
+			let newAnnotation = _.pick(req.body, baseAllowedFields);
+			newAnnotation.publication_id = addedPublication.id; // We're overriding the actual publication ID with a reference
+			newAnnotation.annotation_id = addedSubAnnotation.id;
+
+			return Annotation.forge(newAnnotation).save();
+		})
+		.then(addedAnnotation => {
+			return res.status(201)
+				.json(addedAnnotation);
+		})
+		.catch(err => {
+			res.status(500)
+				.json({
+					error: 'DatabaseError',
+					message: 'Server had an issue adding the annotation'
+				});
+		});
 }
 
 module.exports = {
