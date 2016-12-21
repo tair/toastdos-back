84d6364837595c53a082692e8043cc0508f95280
Joseph DeSimpliciis <jcdesimp@gmail.com>
2016-12-21 17:35:14 -0500

    Allow "Authorization" header. Fixes #29

--- app/middleware/utils.js
+++ app/middleware/utils.js
@@ -2,10 +2,10 @@
 
 function enableCORS(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     return next();
 }
 
 module.exports = {
     enableCORS
-}
+};
