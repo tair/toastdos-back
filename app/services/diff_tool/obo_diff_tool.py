820cef0a85b5ced2c97277b3b130337dd8f2cdd2
Rafael Eduardo Lopez Montilla <rafael.lopez.leaf@gmail.com>
2016-12-22 03:37:56 -0600

    implemented proof-of-concept diff-tool
    Tool prints changed terms as they appear in the newer file of the diff. If applicable also prints a list of deleted terms’ IDs. Code is cleaner than before. Still needs testing for robustness and speed.

--- app/services/diff_tool/obo_diff_tool.py
+++ app/services/diff_tool/obo_diff_tool.py
@@ -1,154 +1,150 @@
 import os
 import string
-from subprocess import *
+# from subprocess import *
+import subprocess
 from sys import argv
 import re
 
+class OboChange:
+
+    def __init__(self, f1, f2, f1_linenum, descriptor, f2_linenum):
+        if not re.match('[acd]',descriptor):
+            print("descriptor: ",descriptor)
+            print("desc length: ", len(self.desc))
+            raise NameError("descriptor is expected to be an 'a', 'c', or 'd' char")
+
+        self.changes_s1 = f1_linenum[0]
+        self.desc = descriptor.strip()
+        self.changes_s2 = f2_linenum[0]
+        self.changedTerms = {}
+        self.deletedTerms = {}
+        if len(f1_linenum) > 1:
+            self.changes_e1 = f1_linenum[1]
+        else:
+            self.changes_e1 = self.changes_s1
+        if len(f2_linenum) > 1:
+            self.changes_e2 = f2_linenum[1]
+        else:
+            self.changes_e2 = self.changes_s2
+
+
+        if descriptor == 'd':
+            self.setDeletedTerms(f1)
+        self.setChangedTerms(f2)
+
+
+
+    def setChangedTerms(self, file_txt):
+        upperBound = self.changes_s2 - 1
+        lowerBound = self.changes_e2 - 1
+
+        # get upper and lower bounds of changed area
+        while file_txt[upperBound].strip() != "[Term]" and file_txt[upperBound].strip() != '':
+            upperBound -= 1
+        while file_txt[lowerBound].strip() != '':
+            lowerBound += 1
+
+        # get oboTerms from file_txt
+        oboTerm = ""
+        oboTermID = None
+
+        for i in range(upperBound, lowerBound + 1):
+            if not oboTermID:
+                if file_txt[i] == '':
+                    continue
+                regex_search = re.search("id: (.+)(?:\n)?",file_txt[i])
+                if regex_search:
+                    oboTermID = regex_search.group(1)
+            elif file_txt[i].strip() == '':
+                if oboTermID in self.deletedTerms:
+                    del(self.deletedTerms[oboTermID])
+                self.changedTerms[oboTermID] = oboTerm
+                oboTerm = ''
+                oboTermID = None
+                continue
+            oboTerm += file_txt[i]
+
+
+
+    def setDeletedTerms(self, file_txt):
+        upperBound = self.changes_s1 - 1
+        lowerBound = self.changes_e1 - 1
+
+        # get upper and lower bounds of changed area
+        while file_txt[upperBound].strip() != "[Term]" and file_txt[upperBound].strip() != '':
+            upperBound -= 1
+        while file_txt[lowerBound].strip() != '':
+            lowerBound += 1
+
+        # get oboTerms from file_txt
+        oboTerm = ""
+        oboTermID = None
+
+        for i in range(upperBound, lowerBound + 1):
+            if not oboTermID:
+                if file_txt[i] == '':
+                    continue
+                regex_search = re.search("id: (.+)(?:\n)?",file_txt[i])
+                if regex_search:
+                    oboTermID = regex_search.group(1)
+            elif file_txt[i].strip() == '':
+                self.deletedTerms[oboTermID] = oboTerm
+                oboTerm = ''
+                oboTermID = None
+                continue
+            oboTerm += file_txt[i]
+
+
+
+
+
+
+
+
 def main(file_path1, file_path2):
 
-    # TODO: make objects instead of awful lists for everything
+    process = subprocess.Popen(["diff",file_path1,file_path2], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    linux_diff = process.stdout.read().decode("utf-8")
 
-    linux_diff = run(["diff",file_path1,file_path2], stdout=PIPE).stdout.decode('utf-8')
-    # print(linux_diff)
-
-    # get acd and line nums
-    acd_list = []
-    f1_lines_list = []
-    f2_lines_list = []
-
-    # get diff's lines that tell changed line numbers
-        # looks like this [##],##(a|c|d)##,[##] e.g. 29a32,34
-    all_ch_descripts = re.findall('((?:[0-9]*,)?[0-9]+[acd][0-9]+(?:,[0-9]*)?)', linux_diff)
-
-    for ch_descript in all_ch_descripts:
-        print(ch_descript)
-        # print()
-        acd = re.search('[acd]',ch_descript).group(0)
-        f1_linenum_s = re.search('((?:[0-9]+,)?[0-9]+)[acd]',ch_descript).group(1)
-        f2_linenum_s = re.search('[acd]([0-9]+(?:,[0-9]+)?)',ch_descript).group(1)
-        print('acd: ',acd)
-        # print('f1_linenum_s: ',f1_linenum_s)
-        # print('f2_linenum_s: ',f2_linenum_s)
-        # print()
-        f1_linenum_n = []
-        f2_linenum_n = []
-        for x in f1_linenum_s.split(','):
-            f1_linenum_n.insert(len(f1_linenum_n),int(x))
-        for y in f2_linenum_s.split(','):
-            f2_linenum_n.insert(len(f2_linenum_n),int(y))
-        # print('f1_linenum_n: ',f1_linenum_n)
-        # print('f2_linenum_n: ',f2_linenum_n)
-        # print()
-        acd_list.insert(len(acd_list),acd)
-        f1_lines_list.insert(len(f1_lines_list),f1_linenum_n)
-        f2_lines_list.insert(len(f2_lines_list),f2_linenum_n)
-        print('len(acd_list): ',len(acd_list))
-        print('acd_list: ',acd_list)
-        # print('f1_lines_list: ',f1_lines_list)
-        # print('f2_lines_list: ',f2_lines_list)
-        print('\n')
+    # get the changes found in the diff
+    list_OboChanges = []
+    list_DeletedIDs = []
 
     # get og files' txt blocks
     file1 = open(file_path1).readlines()
     file2 = open(file_path2).readlines()
 
-    f1_ch_blocks =[]
-    f2_ch_blocks =[]
-    print(len(f1_ch_blocks))
-    print(len(f2_ch_blocks))
+    # get diff's changes' descriptions
+        # looks like this [##],##(a|c|d)##,[##] e.g. 29a32,34
+    all_ch_descripts = re.findall('((?:[0-9]*,)?[0-9]+[acd][0-9]+(?:,[0-9]*)?)', linux_diff)
 
-    # make incomplete txt blocks
-    for i in range(len(acd_list)):
-        curr_block = []
-        print('--',acd_list[i],'--')
-        # print file 1 lines
-        if acd_list[i] == 'c' or acd_list[i] == 'd':
-            # add lines into fx_ch_blocks
-            curr_block.insert(0,(acd_list[i],f1_lines_list[i]))
+    # take change descriptions' descriptor (add, change, del) and the changes'
+    #   line numbers
+    for ch_descript in all_ch_descripts:
+        acd = re.search('[acd]',ch_descript).group(0)
+        f1_linenum_s = re.search('((?:[0-9]+,)?[0-9]+)[acd]',ch_descript).group(1)
+        f2_linenum_s = re.search('[acd]([0-9]+(?:,[0-9]+)?)',ch_descript).group(1)
+        f1_linenum_n = [int(x) for x in f1_linenum_s.split(',')]
+        f2_linenum_n = [int(y) for y in f2_linenum_s.split(',')]
+        # for x in f1_linenum_s.split(','):
+        #     f1_linenum_n.insert(len(f1_linenum_n),int(x))
+        # for y in f2_linenum_s.split(','):
+        #     f2_linenum_n.insert(len(f2_linenum_n),int(y))
+        change = OboChange(file1, file2, f1_linenum_n, acd, f2_linenum_n)
+        list_OboChanges.append(change)
 
-            if len(f1_lines_list[i]) == 1:
-                j = f1_lines_list[i][0]
-                curr_block.insert(len(curr_block),acd_list[i]+' < '+file1[j-1])
-                print('og f1',j, ' -',file1[j-1],end='')
+    # get IDs of deleted terms in a list and print changed terms
+    for change in list_OboChanges:
+        for deletionID in change.deletedTerms:
+            list_DeletedIDs.append(deletionID)
+        for term in change.changedTerms.items():
+            print(term[1])
 
-            elif len(f1_lines_list[i]) > 1:
-                for j in range(f1_lines_list[i][0],f1_lines_list[i][1]):
-                    curr_block.insert(len(curr_block),acd_list[i]+' < '+file1[j-1])
-                    print('og f1', j, ' -', file1[j-1],end='')
-
-            # put txt block in its block list and then empty curr_block
-            f1_ch_blocks.insert(len(f1_ch_blocks),curr_block)
-            curr_block = []
-
-        if acd_list[i] == 'c':
-            print('   -')
-        # print file 2 lines
-        if acd_list[i] == 'c' or acd_list[i] == 'a':
-            curr_block.insert(0,(acd_list[i],f2_lines_list[i]))
-
-            if len(f2_lines_list[i]) == 1:
-                j = f2_lines_list[i][0]
-                curr_block.insert(len(curr_block),acd_list[i]+' > '+file2[j-1])
-                print('og f2',j, ' -',file2[j-1],end='')
-
-            elif len(f2_lines_list[i]) > 1:
-                for j in range(f2_lines_list[i][0],f2_lines_list[i][1]):
-                    curr_block.insert(len(curr_block),acd_list[i]+' > '+file2[j-1])
-                    print('og f2', j, ' -', file2[j-1],end='')
-
-            # put txt block in its block list and then empty curr_block
-            f2_ch_blocks.insert(len(f2_ch_blocks),curr_block)
-            curr_block = []
-
-
-    print('------------------file1')
-    complete_blocks(f1_ch_blocks,file1)
-    print('------------------file2')
-    complete_blocks(f2_ch_blocks,file2)
-
-def complete_blocks(blocks, oboFile):
-    for block in blocks:
-        first_line_indx = block[0][1][0]
-        last_line_indx = block[0][1][len(block[0][1])-1]
-        while block[1].strip() != '[Term]':
-            block.insert(1,'    '+oboFile[first_line_indx])
-            first_line_indx -= 1
-        while oboFile[last_line_indx+1].strip() != '':
-            block.append('    '+oboFile[last_line_indx])
-            last_line_indx += 1
-
-        print(block[0])
-        for x in range(1,len(block)):
-            print(block[x],end='')
-        print()
-
-
-
-    
-
-
-
-
-
-    # match diffs with their blocks in their original thing using regex
-
-
-
-
-
-    # get acd
-    # switch(acd)
-    #     case a:
-    #         get f2start, f2end
-    #         get f2[f2start,f2end] as smoltxt
-    #         if smoltxt != blockstartline
-    #             get b'blockstartline s,oltxt .* \n\n'
-    #         else
-    #             get 'smoltxt .* \n\n'
-
-
-
-
+    # print IDs of deleted terms
+    if len(list_DeletedIDs) > 0:
+        print("-------- entirely deleted terms' IDs --------")
+        for deletionID in list_DeletedIDs:
+            print(deletionID)
 
 
 if __name__ == "__main__":
