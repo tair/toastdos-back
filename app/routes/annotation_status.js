041e2d33714be5572f6d1d2fd2cae586f39a42a2
JDonut <thekillerdonut1@aol.com>
2016-12-27 20:06:23 -0500

    Endpoint for getting all Annotation statuses

---
+++ app/routes/annotation_status.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const annotationStatusController = require('../controllers/annotation_status');
+
+const express = require('express');
+let router = express.Router();
+
+// GET all Annotation statuses
+router.get('/', annotationStatusController.getAnnotationStatuses);
+
+module.exports = router;
