4267dddf2f8b837bb82f6aeda6950108618e6598
James Moretti <thekillerdonut1@aol.com>
2017-02-06 11:04:25 -0500

    Added production configuration file

--- config/development.js
+++ config/development.js
@@ -5,7 +5,6 @@ module.exports = {
 	database: require('../knexfile').development,
 	jwt: {
 		algorithm: 'RS256',
-		expiresIn: '1800000', 		// expire after 1 minute
-		
+		expiresIn: '1800000', // expire after 1 hour
 	}
 };
