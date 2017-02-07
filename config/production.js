43db755baccf27d1b2dda36f9fffdf2620994418
Joseph DeSimpliciis <jcdesimp@gmail.com>
2017-02-06 21:28:52 -0500

    Database connection details ready for production.

--- config/production.js
+++ config/production.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	// production environment configuration
-	database: require('../knexfile').development, // FIXME change to production when we get postgress going in prod
+	database: require('../knexfile').production,
 	jwt: {
 		algorithm: 'RS256',
 		expiresIn: '9000000', // expire after 5 hours
