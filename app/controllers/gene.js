151a9c419b81ec51a26dc3a13aeb4b61253656de
James Moretti <jim5776@rit.edu>
2017-02-11 14:44:46 -0500

    Pull locus verifier out into it's own function

--- app/controllers/gene.js
+++ app/controllers/gene.js
@@ -21,10 +21,6 @@ const UNIPROT_NAME_REGEX     = /^[OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9]([A-Z][
 /**
  * Search external resources for a Gene by its full name.
  *
- * We guess the source of the given Locus name and check that source first.
- * If we can't find the Locus there, we fall back to checking the other sources.
- * This is an optimization to speed up validation.
- *
  * Responds with an 200 if we get a single result.
  * Responds with a 404 if we find nothing.
  *
@@ -33,12 +29,33 @@ const UNIPROT_NAME_REGEX     = /^[OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9]([A-Z][
  * @param  {Function} next - pass to next route handler
  */
 function getByFullName(req, res, next) {
+	verifyLocus(req.params.name)
+		.then(locus => response.ok(res, locus))
+		.catch(err => {
+			if (err.message === `No Locus found for name ${req.params.name}`) {
+				return response.notFound(`No Locus found for name ${req.params.name}`);
+			} else {
+				return response.defaultServerError(res, err);
+			}
+		});
+}
+
+
+/**
+ * Verifies the external source of a Gene.
+ *
+ * We guess the source of the given Locus name and check that source first.
+ * If we can't find the Locus there, we fall back to checking the other sources.
+ * This is an optimization to speed up validation.
+ *
+ * Returns a promise that either resolves with the found locus,
+ * or rejects with an error message.
+ */
+function verifyLocus(name) {
 	let locusLookup;
 	let locusLookupFallback1;
 	let locusLookupFallback2;
 	let locusLookupFallback3;
-
-	let name = req.params.name;
 
 	// Use Locus name to Guess which external source we should query first
 	if (name.match(TAIR_NAME_REGEX)) {
@@ -62,45 +79,48 @@ function getByFullName(req, res, next) {
 		locusLookupFallback3 = RNACental.getLocusByName;
 	}
 
-	// Check the guessed Locus source first
-	if (locusLookup) {
-		return locusLookup(name)
-			.then(locus => response.ok(res, locus))
-			.catch(err => {
-				if (err.contains('No Locus found')) {
-					return Bluebird.any([
-						locusLookupFallback1(name),
-						locusLookupFallback2(name)
-					]);
-				} else {
-					response.defaultServerError(res, err);
-				}
-			})
-			.then(locus => response.ok(res, locus))
-			.catch(aggregateError => {
-				if (aggregateError.every(err => err.contains('No Locus found'))) {
-					response.notFound(res, `No Locus found for name ${name}`);
-				} else {
-					response.defaultServerError(res, err);
-				}
-			});
-	}
-	else {
-		// If we can't reliably guess a source, just search all our external sources for the Locus.
-		return Bluebird.any([
-			locusLookupFallback1(name),
-			locusLookupFallback2(name),
-			locusLookupFallback3(name)
-		])
-			.then(locus => response.ok(res, locus))
-			.catch(aggregateError => {
-				if (aggregateError.every(err => err.contains('No Locus found'))) {
-					response.notFound(res, `No Locus found for name ${name}`);
-				} else {
-					response.defaultServerError(res, err);
-				}
-			});
-	}
+	return new Promise((resolve, reject) => {
+
+		// Check the guessed Locus source first
+		if (locusLookup) {
+			locusLookup(name)
+				.then(locus => resolve(locus))
+				.catch(err => {
+					if (err.contains('No Locus found')) {
+						return Bluebird.any([
+							locusLookupFallback1(name),
+							locusLookupFallback2(name)
+						]);
+					} else {
+						reject(err);
+					}
+				})
+				.then(locus => resolve(locus))
+				.catch(aggregateError => {
+					if (aggregateError.every(err => err.contains('No Locus found'))) {
+						reject(`No Locus found for name ${name}`);
+					} else {
+						reject(err);
+					}
+				});
+		}
+		else {
+			// If we can't reliably guess a source, just search all our external sources for the Locus.
+			Bluebird.any([
+				locusLookupFallback1(name),
+				locusLookupFallback2(name),
+				locusLookupFallback3(name)
+			])
+				.then(locus => resolve(locus))
+				.catch(aggregateError => {
+					if (aggregateError.every(err => err.contains('No Locus found'))) {
+						reject(`No Locus found for name ${name}`);
+					} else {
+						reject(err);
+					}
+				});
+		}
+	});
 }
 
 module.exports = {
