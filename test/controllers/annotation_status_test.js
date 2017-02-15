8bba3c5abdcd8f9b26339c5b952b0267edf517f5
Jean Cheng <jxc3889@rit.edu>
2017-02-14 19:02:01 -0500

    Add created_at column to all the tables. Updated the tests to reflect these changes.

--- test/controllers/annotation_status_test.js
+++ test/controllers/annotation_status_test.js
@@ -2,6 +2,7 @@
 
 const chai = require('chai');
 chai.use(require('chai-http'));
+chai.use(require('chai-subset'));
 
 const server = require('../../app/index');
 const knex    = require('../../app/lib/bookshelf').knex;
@@ -26,7 +27,7 @@ describe('Annotation Status Controller', function() {
 				.get('/api/annotationstatus/')
 				.end((err, res) => {
 					chai.expect(res.status).to.equal(200);
-					chai.expect(res.body).to.deep.equal(testdata.annotation_statuses);
+					chai.expect(res.body).to.containSubset(testdata.annotation_statuses);
 					done();
 				});
 		});
