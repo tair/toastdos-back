f6efb4ebf34cc97ab6346747eba785c9fb321373
JDonut <thekillerdonut1@aol.com>
2017-03-08 02:32:18 -0500

    Add roles to production seed data

--- seeds/prod_data.js
+++ seeds/prod_data.js
@@ -5,6 +5,7 @@ exports.seed = function(knex, Promise) {
 	return Promise.all([
 		...proddata.keyword_types.map(keyword => knex('keyword_type').insert(keyword)),
 		...proddata.annotation_statuses.map(status => knex('annotation_status').insert(status)),
-		...proddata.annotation_types.map(type => knex('annotation_type').insert(type))
+		...proddata.annotation_types.map(type => knex('annotation_type').insert(type)),
+		...proddata.roles.map(role => knex('role').insert(role))
 	]);
 };
