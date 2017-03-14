4ccaae17061d7289cdced3f86eb8e3c4113cf435
JDonut <thekillerdonut1@aol.com>
2017-03-14 08:11:33 -0400

    Promisify obo downloader functions

--- app/services/obo_importer/obo_downloader.js
+++ app/services/obo_importer/obo_downloader.js
@@ -17,30 +17,44 @@ function downloadObo(uri) {
 	let filePath = `${OBO_ROOT}/${filename}`;
 	let fileStream = fs.createWriteStream(filePath);
 
-	request.get(uri)
-		.on('response', res => {
-			if (res.statusCode !== 200) {
-				fileStream.end(() => {
+	let error = null;
+
+	return new Promise((resolve, reject) => {
+		request.get(uri)
+			.on('response', res => {
+				if (res.statusCode !== 200) {
+					error = res.statusMessage;
+					fileStream.end();
+				}
+			})
+			.pipe(fileStream)
+			.on('finish', () => {
+				if (error) {
 					fs.unlinkSync(filePath);
-					throw new Error(`Error downloading '${uri}': ${res.statusMessage}`);
-				});
-			}
-		})
-		.pipe(fileStream);
+					reject(new Error(`Error downloading '${uri}': ${error}`));
+				} else {
+					resolve();
+				}
+			});
+	});
 }
 
 /**
  * Moves the given obo file into the obo cache
  */
 function cacheObo(filename) {
-	fs.renameSync(`${OBO_ROOT}/${filename}`, `${OBO_ROOT}/cache/${filename}`);
+	return new Promise((resolve, reject) => {
+		fs.renameSync(`${OBO_ROOT}/${filename}`, `${OBO_ROOT}/cache/${filename}`, () => resolve());
+	});
 }
 
 /**
  * Restores the given obo file from the obo cache
  */
 function uncacheObo(filename) {
-	fs.renameSync(`${OBO_ROOT}/cache/${filename}`, `${OBO_ROOT}/${filename}`);
+	return new Promise((resolve, reject) => {
+		fs.renameSync(`${OBO_ROOT}/cache/${filename}`, `${OBO_ROOT}/${filename}`, () => resolve());
+	});
 }
 
 /**
