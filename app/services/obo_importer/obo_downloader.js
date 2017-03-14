49041c37f60d2d9f9e1de8c77e3b71c5dd08351b
JDonut <thekillerdonut1@aol.com>
2017-03-14 07:54:17 -0400

    Refactor obo downloader into a module

--- app/services/obo_importer/obo_downloader.js
+++ app/services/obo_importer/obo_downloader.js
@@ -5,15 +5,18 @@ const fs      = require('fs');
 
 const NAME_EXTRACTOR = /.*\/(.*)/;
 const DEFAULT_FILE_NAME = 'default_name';
+const OBO_ROOT = 'resources/obo';
 
 /**
  * Downloads the obo file from the given uri
  */
 function downloadObo(uri) {
-	let filePath = `resources/obo/${uri.match(NAME_EXTRACTOR)[1]}`;
-	if (!filePath) filePath = DEFAULT_FILE_NAME;
+	let filename = filenameFrom(uri);
+	if (!filename) filename = DEFAULT_FILE_NAME;
 
+	let filePath = `${OBO_ROOT}/${filename}`;
 	let fileStream = fs.createWriteStream(filePath);
+
 	request.get(uri)
 		.on('response', res => {
 			if (res.statusCode !== 200) {
@@ -30,12 +33,26 @@ function downloadObo(uri) {
  * Moves the given obo file into the obo cache
  */
 function cacheObo(filename) {
-	fs.renameSync(`resources/obo/${filename}`, `resources/obo/cache/${filename}`);
+	fs.renameSync(`${OBO_ROOT}/${filename}`, `${OBO_ROOT}/cache/${filename}`);
 }
 
 /**
  * Restores the given obo file from the obo cache
  */
 function uncacheObo(filename) {
-	fs.renameSync(`resources/obo/cache/${filename}`, `resources/obo/${filename}`);
+	fs.renameSync(`${OBO_ROOT}/cache/${filename}`, `${OBO_ROOT}/${filename}`);
 }
+
+/**
+ * Extracts the name of the file located at the given uri
+ */
+function filenameFrom(uri) {
+	return uri.match(NAME_EXTRACTOR)[1];
+}
+
+module.exports = {
+	downloadObo,
+	cacheObo,
+	uncacheObo,
+	filenameFrom
+};
