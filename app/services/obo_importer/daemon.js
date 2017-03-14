5654e7dab6742a90a1e902abf94898b4657e09d6
JDonut <thekillerdonut1@aol.com>
2017-03-14 09:57:34 -0400

    Add logging and check for initial diff with md5 hash

--- app/services/obo_importer/daemon.js
+++ app/services/obo_importer/daemon.js
@@ -1,16 +1,35 @@
 'use strict';
 
+const fs      = require('fs');
+const md5file = require('md5-file');
+
 const oboHelper   = require('./obo_downloader');
 const oboImporter = require('./gene_ontology_importer');
 
 const OBO_ROOT = 'resources/obo';
 
 function updateKeywordsUsing(oboURI) {
-	console.log(`Updating from ${oboURI}`);
+	console.log(`Downloading from ${oboURI}`);
 	oboHelper.downloadObo(OBO_ROOT, oboURI).then((oboName) => {
 
-		console.log(`Downloaded ${oboName}, starting update...`);
-		oboImporter.loadOboIntoDB(`${OBO_ROOT}/${oboName}`)
-			.then(() => console.log(`Finished importing ${oboName}`));
+		console.log(`Downloaded ${oboName}`);
+
+		if (fs.existsSync(`${OBO_ROOT}/cache/${oboName}`)) {
+			console.log('Checking for differences with cached version...');
+			if (md5file.sync(`${OBO_ROOT}/${oboName}`) !== md5file.sync(`${OBO_ROOT}/cache/${oboName}`)) {
+
+				console.log('File has changed, importing updates...');
+				oboImporter.loadOboIntoDB(`${OBO_ROOT}/${oboName}`)
+					.then(() => console.log(`Finished importing ${oboName}`));
+			}
+			else {
+				console.log('No difference found. Stopping.');
+			}
+		}
+		else {
+			console.log('No cached version found. Running initial import...');
+			oboImporter.loadOboIntoDB(`${OBO_ROOT}/${oboName}`)
+				.then(() => console.log(`Finished importing ${oboName}`));
+		}
 	});
 }
