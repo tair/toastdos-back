57e8611295e08b0aa3993c4e8ff308cf41c0a648
JDonut <thekillerdonut1@aol.com>
2017-03-24 18:47:07 -0400

    Simplify locus map generation

--- app/controllers/submission.js
+++ app/controllers/submission.js
@@ -80,14 +80,13 @@ function submitGenesAndAnnotations(req, res, next) {
 			.then(([publication, locusArray]) => {
 
 				// Create a map of the locuses / symbols we added so we can quickly look them up by name
-				let locusMap = locusArray.map(([locus, symbol]) => {
-					return {
-						[locus.attributes.locus_name]: {
-							locus: locus,
-							symbol: symbol
-						}
+				let locusMap = locusArray.reduce((acc, [locus, symbol]) => {
+					acc[locus.attributes.locus_name] = {
+						locus: locus,
+						symbol: symbol
 					};
-				}).reduce((accumulator, curValue) => Object.assign(accumulator, curValue));
+					return acc;
+				}, {});
 
 				let annotationPromises = req.body.annotations.map(annotation => {
 					// Every Annotation needs a reference to Publication and User ID
