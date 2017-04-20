8315cd7ce1f87cab92c2ba76547921282ab0292c
Mia Moretti <jim5776@rit.edu>
2017-04-20 06:08:13 -0400

    Ability to pipe single diff groups as one chunk

--- app/services/diff_tool/unix_diff.js
+++ app/services/diff_tool/unix_diff.js
@@ -1,4 +1,30 @@
 const spawn  = require('child_process').spawn;
+const stream = require('stream');
+
+const START_OF_GROUP = /^(?:[0-9]+,)?[0-9]+[acd][0-9]+(?:,[0-9]+)?$/;
+
+/**
+ * Splits chunks of diff output into individual diff groups.
+ * Groups start with the line that shows the line numbers
+ * for a section of the diff.
+ */
+class DiffChunker extends stream.Transform {
+
+	_transform(chunk, encoding, next) {
+		let curGroup;
+
+		chunk.toString().split('\n').forEach(line => {
+			if (line.match(START_OF_GROUP)) {
+				if (curGroup) {
+					this.push(curGroup);
+				}
+				curGroup = '';
+			}
+			curGroup += line + '\n';
+		});
+	}
+
+}
 
 /**
  * A wrapper for the unix diff utility.
@@ -17,4 +43,24 @@ function unixDiff(file1, file2) {
 	return diff.stdout;
 }
 
-module.exports = unixDiff;
+/**
+ * Further process the unix diff output so that each chunk
+ * from the stream contains a single diff group.
+ */
+function chunkedUnixDiff(file1, file2) {
+	let chunker = new DiffChunker();
+	let diff = unixDiff(file1, file2);
+
+	// Forward errors so the consumer can deal with them
+	diff.on('error', err => {
+		chunker.emit('error', err);
+	});
+
+	diff.pipe(chunker);
+	return chunker;
+}
+
+module.exports = {
+	unixDiff,
+	chunkedUnixDiff
+};
