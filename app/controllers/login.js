21aabf8195aeb4648ff91801af194174e00beb1d
Mia Moretti <jim5776@rit.edu>
2017-04-25 23:54:34 -0400

    .get argument needs to be a string. id was undefined here.

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -44,7 +44,7 @@ function login(req, res, next) {
 					userPromise = Promise.resolve(user);
 				}
 				return userPromise.then(resolvedUser => {
-					auth.signToken({user_id: resolvedUser.get(id)}, (err, userJwt) => {
+					auth.signToken({user_id: resolvedUser.get('id')}, (err, userJwt) => {
 						return response.ok(res, {
 							jwt: userJwt
 						});
@@ -61,7 +61,7 @@ function login(req, res, next) {
 						role => newUser.roles().attach(role)
 					).then(
 						() =>
-							auth.signToken({user_id: newUser.get(id)}, (err, userJwt) => {
+							auth.signToken({user_id: newUser.get('id')}, (err, userJwt) => {
 								return response.created(res, {
 									jwt: userJwt
 								});
