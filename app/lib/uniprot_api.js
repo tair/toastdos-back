bdf2664f7d63006a62820ecd20981f8d1dc6a713
Ben Grawi <onionhouseprod@gmail.com>
2017-09-28 20:28:05 -0400

    Address review concerns for uniprot api

--- app/lib/uniprot_api.js
+++ app/lib/uniprot_api.js
@@ -4,10 +4,7 @@ const request = require('request');
 
 const NCBI = require('./ncbi_api');
 
-// ex: For "Vulpes vulpes (red fox)", capture "Vulpes vulpes"
-const TAXON_EXTRACTING_REGEX = /^(\b[\w. ]+\b) *(?:\(.*)?$/;
-const QUERY_RESULT_LIMIT = 10;
-const BASE_URL = 'http://www.ebi.ac.uk/proteins/api/proteins/';
+const BASE_URL = 'https://www.ebi.ac.uk/proteins/api/proteins/';
 
 /**
  * Gets a uniprot locus by name.
@@ -19,25 +16,39 @@ const BASE_URL = 'http://www.ebi.ac.uk/proteins/api/proteins/';
  * @returns {Promise}
  */
 function getLocusByName(name) {
+	let notFoundErrorMessage = `No Locus found for name ${name}`;
 	let requestUrl = `${BASE_URL}${name}.json`;
 	return new Promise((resolve, reject) => {
 		request.get(requestUrl, (error, response, bodyJson) => {
 			if (error) {
 				reject(new Error(error));
-			} else if (response.statusCode != 200) {
-				reject(new Error(`No Locus found for name ${name}`));
+			} else if (response.statusCode >= 400) {
+				reject(new Error(notFoundErrorMessage));
 			} else {
 				let body = JSON.parse(bodyJson);
-				let taxonName = body.organism.names[0].value;
+				let taxonName = null;
+				let names = body && body.organism && body.organism.names ? body.organism.names : null;
 
-				NCBI.getTaxonByScientificName(taxonName).then(taxonInfo => {
-					resolve({
-						source: 'Uniprot',
-						locus_name: body.accession,
-						taxon_name: taxonName,
-						taxon_id: taxonInfo.taxId
+				if (names != null) {
+					for (let item of names) {
+						if (item.type && item.type == 'scientific') {
+							taxonName = item.value;
+						}
+					}
+				}
+
+				if (taxonName == null) {
+					reject(new Error(notFoundErrorMessage));
+				} else {
+					NCBI.getTaxonByScientificName(taxonName).then(taxonInfo => {
+						resolve({
+							source: 'Uniprot',
+							locus_name: body.accession,
+							taxon_name: taxonName,
+							taxon_id: taxonInfo.taxId
+						});
 					});
-				});
+				}
 			}
 		});
 	});
