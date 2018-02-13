6ffb32a2e8f267e045ee8b73f5bfcab0801f4bd5
Ben Grawi <onionhouseprod@gmail.com>
2018-02-13 06:59:13 -0500

    Added JSON export for comments on gene data

--- app/services/export_data.js
+++ app/services/export_data.js
@@ -42,7 +42,7 @@ function pad(n){
 /**
  * Exports all the data in a GAF file format
  */
-function exportAllData() {
+function exportAnnotations() {
     // TODO Better date generation
     const date = new Date().toISOString().split("T")[0];
     const filePath = EXPORTS_ROOT + '/' + date + '.gaf';
@@ -235,7 +235,7 @@ function exportAllData() {
             return new Promise((resolve, reject) => {
                 fileStream
                     .on('finish', () => {
-                        console.log('Sucessfully finished writing the export file');
+                        console.log('Sucessfully finished writing the GAF export file');
                         resolve();
                     });
             });
@@ -243,7 +243,7 @@ function exportAllData() {
         .catch((error) => {
             // If there was an error, delete the file to avoid incomplete exported files.
             fileStream.end();
-            console.log(`Error while creating export, deleting export file...`);
+            console.log(`Error while GAF creating export, deleting GAF export file...`);
             fs.unlinkSync(filePath);
 
             // Re-throw the promise so we exit with a status of 1.
@@ -251,4 +251,133 @@ function exportAllData() {
         });
 }
 
-module.exports = exportAllData;
+function exportSupplimentalData() {
+    const date = new Date().toISOString().split("T")[0];
+    const filePath = EXPORTS_ROOT + '/' + date + '.json';
+
+    // TODO reduce duplication of initial d
+    return Submission
+        .query(() => {})
+        .fetchAll({withRelated: [
+            'publication',
+            'submitter',
+            'annotations.childData',
+            'annotations.status',
+            'annotations.locus.names',
+            'annotations.locus.names.source',
+            'annotations.locusSymbol',]})
+        .then(submissions => {
+            console.log(`Got ${submissions.length} total submissions`);
+
+            let sortedSubCollection = submissions.sortBy(elem => {
+                // Sort each submission by the date it was created
+                let milliseconds = Date.parse(elem.get('created_at'));
+                return  -milliseconds;
+            }).filter(sub => {
+                // Only include submission's who's annotations are all not pending.
+                let annotations = sub.related('annotations');
+                let pending = annotations.filter(ann => ann.related('status').get('name') === PENDING_STATUS).length;
+                return (pending == 0);
+            });
+
+            console.log(`Got ${sortedSubCollection.length} completed submissions`);
+
+            // Make a flat array for each annotation
+            const annotationsToExport = [];
+            sortedSubCollection.map(submission => {
+                let annotationPromises = submission
+                    .related('annotations')
+                    .filter(ann => ann.related('status').get('name') === ACCEPTED_STATUS)
+                    .map(annotation => {
+                        annotationsToExport.push(annotation.load([
+                            'publication',
+                            'locus.names',
+                            'locus.names.source',
+                            'childData',
+                        ]));
+                    });
+            });
+            return Promise.all(annotationsToExport);
+        }).then(annotations => {
+
+            const lociMap = {};
+            annotations.map(annotation => {
+                // TODO remove duplicated code
+                let DBReference;
+                let publication = annotation.related('publication');
+                if (publication.get('pubmed_id')) {
+                    DBReference = 'PMID:' + publication.get('pubmed_id');
+                } else if (publication.get('doi')) {
+                    DBReference = 'DOI:' + publication.get('doi');
+                } else {
+                    throw new Error("No publication reference id set");
+                }
+
+                const locusName = annotation.related('locus').related('names').shift();
+                const locusKey = annotation.related('locus').get('id') + ':' + DBReference;
+                
+                if (!lociMap.hasOwnProperty(locusKey)) {
+                    const locusSymbol = annotation.related('locusSymbol');
+                    const externalSourceName = locusName.related('source').get('name')
+                    const sourceData = externalSourceData[externalSourceName];
+
+                    let DBObjectName = null;
+                    if (locusSymbol && locusSymbol.get('full_name')) {
+                        DBObjectName = locusSymbol.get('full_name');
+                    }
+                    let DBObjectSymbol = null;
+                    if (locusSymbol && locusSymbol.get('symbol')) {
+                        DBObjectSymbol = locusSymbol.get('symbol')
+                    }
+                
+                    lociMap[locusKey] = {
+                        publication: DBReference,
+                        locus: locusName.get('locus_name'),
+                        source: sourceData.db,
+                        symbolicName: DBObjectSymbol,
+                        fullName: DBObjectName,
+                        comments: [],
+                    };
+                }
+
+                if (annotation.get('annotation_format') === 'comment_annotation') {
+                    lociMap[locusKey].comments.push(annotation.related('childData').get('text'));
+                }
+            });
+            const additionalData = [];
+            for (let locusKey in lociMap) {
+                additionalData.push(lociMap[locusKey]);
+            }
+
+            return new Promise((resolve, reject) => {
+
+                fs.writeFile(filePath, JSON.stringify(additionalData), 'utf8', (err) => {
+                    if(!err) {
+                        console.log('Sucessfully finished writing the json export file');
+                        resolve();
+                    } else {
+                        reject(err);
+                    }
+                });
+            });
+        })
+        .catch((error) => {
+            // If there was an error, delete the file to avoid incomplete exported files.
+            console.log(`Error while creating json export, deleting json export file...`);
+            fs.unlinkSync(filePath);
+
+            // Re-throw the promise so we exit with a status of 1.
+            return Promise.reject(error);
+        });
+
+}
+
+function exportAllData() {
+    return exportAnnotations().then(exportSupplimentalData);
+}
+
+module.exports = {
+    exportAllData,
+    exportAnnotations,
+    exportSupplimentalData
+};
