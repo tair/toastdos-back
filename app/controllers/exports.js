fd27217756f6e0c7cd68f92fe5683d6753ea903f
Ben Grawi <onionhouseprod@gmail.com>
2018-02-16 13:19:43 -0500

    Added exports controller and static file server

---
+++ app/controllers/exports.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const response = require('../lib/responses');
+const config = require('../../config');
+const path = require('path');
+const fs = require('fs');
+
+const EXPORTS_PATH = path.join(config.resourceRoot, 'exports');
+
+/**
+ * Gets a list of the exports
+ *
+ * Responses:
+ * 200 json description of the exports
+ * 500 can't read the exports directory
+ */
+function list(req, res, next) {
+	fs.readdir(EXPORTS_PATH, function(err, items) {
+		if (!err) {
+			response.ok(res, items);
+		} else {
+			response.serverError(res, "Error reading exports directory.");
+		}
+	});
+}
+
+
+module.exports = {
+	list
+};
