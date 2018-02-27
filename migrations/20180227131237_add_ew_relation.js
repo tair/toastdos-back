647d03d751d2fb0b9dda120b6bf1b4a1d4a93395
Ben Grawi <onionhouseprod@gmail.com>
2018-02-27 15:05:03 -0500

    Makes the default relation be or

--- migrations/20180227131237_add_ew_relation.js
+++ migrations/20180227131237_add_ew_relation.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
     return knex.schema
 		.table('gene_term_annotation', (table) => {
-            table.boolean('is_evidence_with_or').defaultTo(false);
+            table.boolean('is_evidence_with_or').defaultTo(true);
         });
 };
 
