739ab6f61940fdab3bc0679bd5fff7ddad7433c7
Arron Reed <ajr1644@rit.edu>
2018-04-23 19:00:46 -0400

    email refactor

--- app/lib/send_email.js
+++ app/lib/send_email.js
@@ -6,71 +6,71 @@ var configFile = process.env.RESOURCEROOT + "/aws_ses_config.json";
 AWS.config.loadFromPath(configFile);
 
 function createSubMessage(toEmail, paper) {
-    if (process.env.NODE_ENV != "test") {
-        if (toEmail != "" && toEmail != null) {
-            let params = {
-                Destination: { /* required */
-                    CcAddresses: [],
-                    ToAddresses: [
-                        toEmail
-                    ]
-                },
-                Message: { /* required */
-                    Body: { /* required */
-                        Html: {
-                            Charset: "UTF-8",
-                            Data: "<p>Success! Your annotations for " + paper + " have been submitted for review. Once they are approved, you will be notified. If we have any questions, we will contact you.</p><p>Sincerely,</p><p>Phoenix Bioinformatics</p>"
-                        },
-                        Text: {
-                            Charset: "UTF-8",
-                            Data: "Success! Your annotations for " + paper + " have been submitted for review. Once they are approved, you will be notified. If we have any questions, we will contact you. Sincerely, Phoenix Bioinformatics"
-                        }
+    if (!toEmail || process.env.NODE_ENV == "test") {
+        return;
+    }
+    sendWithParams(
+        {
+            Destination: { /* required */
+                CcAddresses: [],
+                ToAddresses: [
+                    toEmail
+                ]
+            },
+            Message: { /* required */
+                Body: { /* required */
+                    Html: {
+                        Charset: "UTF-8",
+                        Data: "<p>Success! Your annotations for " + paper + " have been submitted for review. Once they are approved, you will be notified. If we have any questions, we will contact you.</p><p>Sincerely,</p><p>Phoenix Bioinformatics</p>"
                     },
-                    Subject: {
-                        Charset: 'UTF-8',
-                        Data: 'Successful Submission on GOAT'
+                    Text: {
+                        Charset: "UTF-8",
+                        Data: "Success! Your annotations for " + paper + " have been submitted for review. Once they are approved, you will be notified. If we have any questions, we will contact you. Sincerely, Phoenix Bioinformatics"
                     }
                 },
-                Source: 'curator@phoenixbioinformatics.org', /* required */
-                ReplyToAddresses: [],
-            };
-            sendWithParams(params);
+                Subject: {
+                    Charset: 'UTF-8',
+                    Data: 'Successful Submission on GOAT'
+                }
+            },
+            Source: 'curator@phoenixbioinformatics.org', /* required */
+            ReplyToAddresses: [],
         }
-    }
+    );
 }
 
 function createCurMessage(toEmail, paper) {
-    if (process.env.NODE_ENV != "test") {
-        if (toEmail != "" && toEmail != null) {
-            let params = {
-                Destination: { /* required */
-                    CcAddresses: [],
-                    ToAddresses: [
-                        toEmail
-                    ]
-                },
-                Message: { /* required */
-                    Body: { /* required */
-                        Html: {
-                            Charset: "UTF-8",
-                            Data: "<p>Congratulations, your annotations for " + paper + " have been approved! They are now included in the data download files which are accessible to users via the 'Download Data' section of our website. </p><p>Thank you for your contributions. </p><p>Sincerely, </p><p>Phoenix Bioinformatics.</p>"
-                        },
-                        Text: {
-                            Charset: "UTF-8",
-                            Data: "Congratulations, your annotations for " + paper + " have been approved! They are now included in the data download files which are accessible to users via the 'Download Data' section of our website. Thank you for your contributions. Sincerely, Phoenix Bioinformatics"
-                        }
+    if (!toEmail || process.env.NODE_ENV == "test") {
+        return;
+    }
+    sendWithParams(
+        {
+            Destination: { /* required */
+                CcAddresses: [],
+                ToAddresses: [
+                    toEmail
+                ]
+            },
+            Message: { /* required */
+                Body: { /* required */
+                    Html: {
+                        Charset: "UTF-8",
+                        Data: "<p>Congratulations, your annotations for " + paper + " have been approved! They are now included in the data download files which are accessible to users via the 'Download Data' section of our website. </p><p>Thank you for your contributions. </p><p>Sincerely, </p><p>Phoenix Bioinformatics.</p>"
                     },
-                    Subject: {
-                        Charset: 'UTF-8',
-                        Data: 'Successful Curation on GOAT'
+                    Text: {
+                        Charset: "UTF-8",
+                        Data: "Congratulations, your annotations for " + paper + " have been approved! They are now included in the data download files which are accessible to users via the 'Download Data' section of our website. Thank you for your contributions. Sincerely, Phoenix Bioinformatics"
                     }
                 },
-                Source: 'curator@phoenixbioinformatics.org', /* required */
-                ReplyToAddresses: [],
-            };
-            sendWithParams(params);
+                Subject: {
+                    Charset: 'UTF-8',
+                    Data: 'Successful Curation on GOAT'
+                }
+            },
+            Source: 'curator@phoenixbioinformatics.org', /* required */
+            ReplyToAddresses: [],
         }
-    }
+    );
 }
 
 function sendWithParams(params) {
