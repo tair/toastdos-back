b9bd636be9ebb411bf0f34aa74cad9de7597d487
Arron Reed <ajr1644@rit.edu>
2018-04-23 18:30:29 -0400

    Send email updates

--- app/lib/send_email.js
+++ app/lib/send_email.js
@@ -2,77 +2,70 @@
 var AWS = require('aws-sdk');
 // Set the region
 var configFile = process.env.RESOURCEROOT + "/aws_ses_config.json";
-var params;
 
 AWS.config.loadFromPath(configFile);
 
 function createSubMessage(toEmail, paper) {
-    params = {
-        Destination: { /* required */
-            CcAddresses: [],
-            ToAddresses: [
-                toEmail
-            ]
-        },
-        Message: { /* required */
-            Body: { /* required */
-                Html: {
-                    Charset: "UTF-8",
-                    Data: "<p>Success! Your annotations for " + paper + " have been submitted for review. Once they are approved, you will be notified. If we have any questions, we will contact you.</p><p>Sincerely,</p><p>Phoenix Bioinformatics</p>"
+    if (toEmail != "" && toEmail != null) {
+        let params = {
+            Destination: { /* required */
+                CcAddresses: [],
+                ToAddresses: [
+                    toEmail
+                ]
+            },
+            Message: { /* required */
+                Body: { /* required */
+                    Html: {
+                        Charset: "UTF-8",
+                        Data: "<p>Success! Your annotations for " + paper + " have been submitted for review. Once they are approved, you will be notified. If we have any questions, we will contact you.</p><p>Sincerely,</p><p>Phoenix Bioinformatics</p>"
+                    },
+                    Text: {
+                        Charset: "UTF-8",
+                        Data: "Success! Your annotations for " + paper + " have been submitted for review. Once they are approved, you will be notified. If we have any questions, we will contact you. Sincerely, Phoenix Bioinformatics"
+                    }
                 },
-                Text: {
-                    Charset: "UTF-8",
-                    Data: "Success! Your annotations for " + paper + " have been submitted for review. Once they are approved, you will be notified. If we have any questions, we will contact you. Sincerely, Phoenix Bioinformatics"
+                Subject: {
+                    Charset: 'UTF-8',
+                    Data: 'Successful Submission on GOAT'
                 }
             },
-            Subject: {
-                Charset: 'UTF-8',
-                Data: 'Successful Submission on GOAT'
-            }
-        },
-        Source: 'curator@phoenixbioinformatics.org', /* required */
-        ReplyToAddresses: [],
-    };
-
-    if (toEmail != "" && toEmail != null) {
+            Source: 'curator@phoenixbioinformatics.org', /* required */
+            ReplyToAddresses: [],
+        };
         sendWithParams(params);
-    } else {
-        console.log("No email for user, not sending message.");
     }
 }
 
 function createCurMessage(toEmail, paper) {
-    params = {
-        Destination: { /* required */
-            CcAddresses: [],
-            ToAddresses: [
-                toEmail
-            ]
-        },
-        Message: { /* required */
-            Body: { /* required */
-                Html: {
-                    Charset: "UTF-8",
-                    Data: "<p>Congratulations, your annotations for " + paper + " have been approved! They are now included in the data download files which are accessible to users via the 'Download Data' section of our website. </p><p>Thank you for your contributions. </p><p>Sincerely, </p><p>Phoenix Bioinformatics.</p>"
+    if (toEmail != "" && toEmail != null) {
+        let params = {
+            Destination: { /* required */
+                CcAddresses: [],
+                ToAddresses: [
+                    toEmail
+                ]
+            },
+            Message: { /* required */
+                Body: { /* required */
+                    Html: {
+                        Charset: "UTF-8",
+                        Data: "<p>Congratulations, your annotations for " + paper + " have been approved! They are now included in the data download files which are accessible to users via the 'Download Data' section of our website. </p><p>Thank you for your contributions. </p><p>Sincerely, </p><p>Phoenix Bioinformatics.</p>"
+                    },
+                    Text: {
+                        Charset: "UTF-8",
+                        Data: "Congratulations, your annotations for " + paper + " have been approved! They are now included in the data download files which are accessible to users via the 'Download Data' section of our website. Thank you for your contributions. Sincerely, Phoenix Bioinformatics"
+                    }
                 },
-                Text: {
-                    Charset: "UTF-8",
-                    Data: "Congratulations, your annotations for " + paper + " have been approved! They are now included in the data download files which are accessible to users via the 'Download Data' section of our website. Thank you for your contributions. Sincerely, Phoenix Bioinformatics"
+                Subject: {
+                    Charset: 'UTF-8',
+                    Data: 'Successful Curation on GOAT'
                 }
             },
-            Subject: {
-                Charset: 'UTF-8',
-                Data: 'Successful Curation on GOAT'
-            }
-        },
-        Source: 'curator@phoenixbioinformatics.org', /* required */
-        ReplyToAddresses: [],
-    };
-
-    if (toEmail != "" && toEmail != null) {
+            Source: 'curator@phoenixbioinformatics.org', /* required */
+            ReplyToAddresses: [],
+        };
         sendWithParams(params);
-    } else {
-        console.log("No email for user, not sending message.");
     }
 }
 
